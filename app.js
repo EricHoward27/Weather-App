f74a86fb0d289588cf1a5e176a76a5173ca91f26
Eric Howard <erichoward2141@gmail.com>
2020-01-07 23:34:31 -0600

    app.js cors update

--- app.js
+++ app.js
@@ -11,16 +11,12 @@
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = '${proxy}https://api.darksky.net/forecast/cb89a804d2f77ba9bfe22f6c04288204/${lat},${long}';
+            var proxy = 'https://cors-anywhere.herokuapp.com/';
+            var apiLinkDS = "https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/38,-122";
 
-        
-        fetch(api)
-        .then(response =>{
-            return response.json()
-        })
-        .then(data => {
-            console.log(data)
+            $.ajax({
+                url: proxy + apiLinkDS,
+                success: function (data) { console.log(data); }
         });
     });
     } else{
