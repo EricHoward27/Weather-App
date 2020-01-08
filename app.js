2858e21ef456aff34cc6aaff6f575e409f03b108
Eric Howard <erichoward2141@gmail.com>
2020-01-07 23:37:44 -0600

    update app.js cors code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
             var proxy = 'https://cors-anywhere.herokuapp.com/';
             var apiLinkDS = "https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/38,-122";
 
-            $.ajax({
+            position.ajax({
                 url: proxy + apiLinkDS,
                 success: function (data) { console.log(data); }
         });
