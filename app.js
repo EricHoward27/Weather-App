5fb85525f683c53a4097287f5f569ee966e1b792
Eric Howard <erichoward2141@gmail.com>
2020-01-12 00:00:41 -0600

    app.js api changes

--- app.js
+++ app.js
@@ -11,16 +11,18 @@
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
+            $(document).ready(function() {
             var proxy = 'https://cors-anywhere.herokuapp.com/';
             var apiLinkDS = "https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/38,-122";
 
-            position.ajax({
+            $.ajax({
                 url: proxy + apiLinkDS,
                 success: function (data) { console.log(data); }
+             
         });
     });
-    } else{
-        h1.textcontent = "Your weather cannot be access without location services!"
+
+});
     }
 });
 
