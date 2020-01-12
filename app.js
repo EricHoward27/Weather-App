f0ecb9151426876724a76e489f57543aa843cdf5
Eric Howard <erichoward2141@gmail.com>
2020-01-12 00:07:27 -0600

    Commit init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*eslint-disable */
 //@ts-nocheck1
-let $;
+
   let long;
   let lat;
     window.addEventListener('load',()=>{
@@ -11,11 +11,11 @@ let $;
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            $(document).ready(function() {
+            (document).ready(function() {
             var proxy = 'https://cors-anywhere.herokuapp.com/';
             var apiLinkDS = "https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/38,-122";
 
-            $.ajax({
+            document.ajax({
                 url: proxy + apiLinkDS,
                 success: function (data) { console.log(data); }
              
