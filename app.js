a609d506811cca7aa5083adb23677396555b97ae
Eric Howard <erichoward2141@gmail.com>
2020-01-12 01:08:58 -0600

    commit init

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+            var proxyUrl = 'https://crossorigin.me/',
              apiLinkDS = "https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/38,-122${long}${lat}"
 
             
