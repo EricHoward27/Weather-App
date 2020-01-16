bd68f3be7bbeb98da50f0606ee609b7aef12dbee
Eric Howard <erichoward2141@gmail.com>
2020-01-15 22:56:55 -0600

    init commit

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 
 }
-const DARK_API_KEY = process.env.DARK_API_KEY
-const express = require('express')
-const app = express()
+var DARKSKY_API_KEY = process.env.DARK_API_KEY
+var path = require('path')
+var axios = require('axios')
+var express = require('express')
+var app = express()
 
-app.use(express.json())
-app.use(express.static('public'))
 
-app.post('/weather', (req, res) => {
-console.log(req.body)
+//static folder
+app.use(express.static(path.join(__dirname, '/')));
+
+app.post('/', (req, res) => {
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+axios({
+    url: url,
+    responseType: 'json'
+}).then(data => res.json(data.data.currently))
+//setWeatherData(data, place.formatted_address)
 })
 
-app.listen(5500, () => {
-    console.log('Server Starting')
-})
+const PORT = process.env.Port || 7000;
+app.listen(PORT, () => console.log(`Sever started on port ${PORT}`))
